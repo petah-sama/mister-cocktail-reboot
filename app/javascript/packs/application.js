ee32c2c3a3253339b53aac23c7d07a988e6529ad
Pedro Batista <pedrobatista.420@gmail.com>
2021-03-24 15:35:56 +0000

    select2 added to ingredient dropdown

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -10,6 +10,12 @@ require("channels")
 
 import 'bootstrap';
 
+import { initSelect2 } from '../components/init_select2';
+
+document.addEventListener("turbolinks:load", function() {
+  initSelect2();
+});
+
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
 // or the `imagePath` JavaScript helper below.
